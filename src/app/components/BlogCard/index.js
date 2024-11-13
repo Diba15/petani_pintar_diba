73904e94b3bbad261a7e6ffc00cf1ed8a8c5895f
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2024-11-13 17:19:28 +0700

    fix BlogCard

--- src/app/components/BlogCard/index.js
+++ src/app/components/BlogCard/index.js
@@ -3,10 +3,11 @@ import blog2 from "../../images/blog2.png";
 import blog3 from "../../images/blog3.png";
 import blog4 from "../../images/blog4.png";
 import Image from "next/image";
+import {Divider} from "@nextui-org/react";
 
 const Card = ({img, title, desc}) => {
     return (
-        <div className="max-w-md mx-auto bg-white rounded-xl mb-5 shadow-md overflow-hidden items-center justify-center lg:max-w-xl lg:mx-10">
+        <div className="max-w-sm mx-auto rounded-xl mb-5 overflow-hidden items-center justify-center md:max-w-xl md:mx-10">
             <div className="md:flex">
                 <div className="md:shrink-0">
                     <Image className="h-48 w-full object-cover md:h-full md:w-60" src={img} alt={"Blog1"}/>
@@ -14,7 +15,6 @@ const Card = ({img, title, desc}) => {
                 <div className="p-8">
                     <a href=""
                        className="block text-start mt-1 text-lg leading-tight font-medium text-black hover:underline">{title}</a>
-                    <p className="mt-2 text-justify text-slate-500 line-clamp-4">{desc}</p>
                 </div>
             </div>
         </div>
@@ -47,10 +47,23 @@ const BlogCard = () => {
     ]
 
     return (
-        <div className={"flex flex-col flex-wrap my-5 justify-center lg:flex-row"}>
-            {blogs.map((blog, index) => (
-                <Card key={index} {...blog}/>
-            ))}
+        <div className={"flex flex-col flex-wrap mx-5 my-5 justify-center lg:flex-row"}>
+            <div className={"flex flex-col"}>
+                {blogs.map((blog, index) => (
+                    index <= 1 && (
+                        <Card key={index} {...blog}/>
+                    )
+                ))}
+            </div>
+            <Divider className={"hidden lg:block"} orientation="vertical"/>
+            <div className={"flex flex-col"}>
+                {blogs.map((blog, index) => (
+                    index > 1 && index <= 3 && (
+                        <Card key={index} {...blog}/>
+                    )
+                ))}
+            </div>
+
         </div>
     )
 }
